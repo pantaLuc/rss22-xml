6f3fb5fdafa20df12398738510d18f7ddf1eb687
Luc Perin Panta <lucpanta@MacBook-Pro-de-Luc.local>
2022-04-15 20:28:19 +0200

    initialiser le depot

---
+++ src/main/java/fr/univrouen/rss22xml/Rss22XmlApplication.java
@@ -0,0 +1,13 @@
+package fr.univrouen.rss22xml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rss22XmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rss22XmlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/univrouen/rss22xml/Rss22XmlApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.univrouen.rss22xml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rss22XmlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
