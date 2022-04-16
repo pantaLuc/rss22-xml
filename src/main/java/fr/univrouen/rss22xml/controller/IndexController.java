3e414b2a81f0d95fea9fe7f1d655165ba0e7de07
Luc Perin Panta <lucpanta@MacBook-Pro-de-Luc.local>
2022-04-16 16:23:04 +0200

    adding some pages

--- src/main/java/fr/univrouen/rss22xml/controller/IndexController.java
+++ src/main/java/fr/univrouen/rss22xml/controller/IndexController.java
@@ -1,27 +1,26 @@
 package fr.univrouen.rss22xml.controller;
 
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Date;
 import java.text.SimpleDateFormat;
 
-@RestController
+@Controller
 public class IndexController {
-    @GetMapping("/" )
-    public String getIndex(Model model) {
-// Initialisation du message
-        String message = "Test Thymeleaf !";
-// Mise en forme de la date
-        SimpleDateFormat format = new SimpleDateFormat("dd MMMM yyyy");
-        String today = format.format(new Date());
-// Création des attributs pour insertion dans la page HTML via Thymeleaf
-        model.addAttribute("message", message);
-        model.addAttribute("today", today);
-// Création de la page HTML avec le template "accueil.html"
-        return "accueil";
+    @RequestMapping(value = "/index" , method = RequestMethod.GET)
+    String getIndex(Model model) {
+        model.addAttribute("something" ,"comming from the controller");
+        return "index";
+    }
+    @RequestMapping(value="/help" ,method = RequestMethod.GET)
+    String getHelp(Model model){
+        return "help";
     }
 }
