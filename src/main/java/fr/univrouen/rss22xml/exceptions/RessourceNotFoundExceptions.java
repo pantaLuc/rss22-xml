28758b4ede44c1768b1cc03db58f73c905b57f15
Luc Perin Panta <lucpanta@MacBook-Pro-de-Luc.local>
2022-04-16 20:57:23 +0200

    adding  class Exception to manage exception Not found

---
+++ src/main/java/fr/univrouen/rss22xml/exceptions/RessourceNotFoundExceptions.java
@@ -0,0 +1,9 @@
+package fr.univrouen.rss22xml.exceptions;
+
+public class RessourceNotFoundExceptions extends Exception{
+    private static  final long serialVersionUUID=1L;
+    public RessourceNotFoundExceptions(String message){
+        super(message);
+    }
+
+}
